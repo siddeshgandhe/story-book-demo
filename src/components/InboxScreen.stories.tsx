450b279d08324cfcc86ef7874afc4d6d52bff476
Siddesh Gandhe <s.gandhe@easternenterprise.com>
2025-07-03 17:07:31 +0530

    Actual API call and mocking API responses

---
+++ src/components/InboxScreen.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import InboxScreen from "./InboxScreen";
+
+import store from "../lib/store";
+
+import { Provider } from "react-redux";
+
+import { http, HttpResponse } from "msw";
+
+import { MockedState } from "./TaskList.stories";
+
+const meta = {
+  component: InboxScreen,
+  title: "InboxScreen",
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+  tags: ["autodocs"],
+} satisfies Meta<typeof InboxScreen>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("https://jsonplaceholder.typicode.com/todos?userId=1", () => {
+          return HttpResponse.json(MockedState.tasks);
+        }),
+      ],
+    },
+  },
+};
+
+export const Error: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("https://jsonplaceholder.typicode.com/todos?userId=1", () => {
+          return new HttpResponse(null, {
+            status: 403,
+          });
+        }),
+      ],
+    },
+  },
+};
